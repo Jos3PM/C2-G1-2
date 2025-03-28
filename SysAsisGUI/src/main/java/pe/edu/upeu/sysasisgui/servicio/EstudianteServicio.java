3e2e80424336e2e0cbb42272d1122ad80e8d7f3a
Jos3PM <just76111@hotmail.com>
2025-03-28 13:07:55 -0500

    Add files via upload

---
+++ SysAsisGUI/src/main/java/pe/edu/upeu/sysasisgui/servicio/EstudianteServicio.java
@@ -0,0 +1,43 @@
+package pe.edu.upeu.sysasisgui.servicio;
+
+import org.springframework.stereotype.Service;
+import pe.edu.upeu.sysasisgui.modelo.Estudiante;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EstudianteServicio {
+
+    public List<Estudiante> estudiantes = new ArrayList<>();
+
+    public void save(Estudiante estudiante) {
+        estudiantes.add(estudiante);
+    }
+
+    public List<Estudiante> getEntidad() {
+        return estudiantes;
+    }
+
+    public void update(int index, Estudiante estudiante) {
+        estudiantes.set(index, estudiante);
+    }
+
+    public void delete(int index) {
+        estudiantes.remove(index);
+    }
+
+    public void deleteEntidad(Estudiante index) {
+        estudiantes.remove(index);
+    }
+
+    public void cargarDatos() {
+        estudiantes.add(new Estudiante("juan"));
+        estudiantes.add(new Estudiante("flores"));
+        estudiantes.add(new Estudiante("damio"));
+        estudiantes.add(new Estudiante("cain"));
+        estudiantes.add(new Estudiante("cruz"));
+        estudiantes.add(new Estudiante("remi"));
+    }
+
+}
