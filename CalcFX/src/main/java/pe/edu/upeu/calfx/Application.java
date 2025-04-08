29eea9666fb7902e156a00106bdf0480af2fecdd
Jos3PM <Just76111@hotmail.com>
2025-04-08 10:54:01 -0500

    proyecto

---
+++ CalcFX/src/main/java/pe/edu/upeu/calfx/Application.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.calfx;
+
+public class Application {
+    public static void main(String[] args) {
+        CalcFxApplication.main(args);
+    }
+}
---
+++ CalcFX/src/main/java/pe/edu/upeu/calfx/control/CalcfxControl.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.calfx.control;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CalcfxControl {
+}
